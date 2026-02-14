c037b417eac7bf9e502a3caa61e0b4099c951f73
Albert Jacf <jrcayetano592@gmail.com>
2026-02-13 20:22:01 -0600

    intentando

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -2,18 +2,24 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$host = getenv('MYSQLHOST');
-$db   = getenv('MYSQLDATABASE');
-$user = getenv('MYSQLUSER');
-$pass = getenv('MYSQLPASSWORD');
-$port = getenv('MYSQLPORT');
+$host = "centerbeam.proxy.rlwy.net";
+$db   = "railway";
+$user = "root";
+$pass = "IhuRwcGRLUNLGFTZqkcNTKJrQdIULpPD";
+$port = "43484";
 
 try {
     $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
 
     $conn = new PDO($dsn, $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false
     ]);
+
+    echo "Conexión exitosa 🚀";
+
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
+?>
