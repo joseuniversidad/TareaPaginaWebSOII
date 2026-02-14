c403b9929c6b10532382e42786cc401cb533b789
Albert Jacf <jrcayetano592@gmail.com>
2026-02-13 21:34:33 -0600

    Pruebas

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,12 @@
-<nav>
+<nav class="navbar">
     <div class="logo">SERVIDORES</div>
 
-    <div class="menu">
+    <!-- Botón hamburguesa -->
+    <div class="hamburger" onclick="toggleMenu()">
+        &#9776;
+    </div>
+
+    <div class="menu" id="menu">
         <ul>
             <li>
                 <a href="../por_funcion/index.php">Por función</a>
@@ -20,7 +25,6 @@
                     <li><a href="../por_funcion/game.php">VideoJuegos</a></li>
                 </ul>
             </li>
-
             <li>
                 <a href="../por_acce/index.php">Acceso</a>
                 <ul>
@@ -30,7 +34,6 @@
                     <li><a href="../por_acce/local.php">Local</a></li>
                 </ul>
             </li>
-
             <li>
                 <a href="../por_imple/index.php">Implementación</a>
                 <ul>
@@ -38,7 +41,6 @@
                     <li><a href="../por_imple/nube.php">Nube</a></li>
                 </ul>
             </li>
-
             <li>
                 <a href="../por_tamaño/index.php">Tamaño</a>
                 <ul>
@@ -47,7 +49,6 @@
                     <li><a href="../por_tamaño/trabajo.php">Trabajo</a></li>
                 </ul>
             </li>
-
             <li>
                 <a href="../por_hard/index.php">Hardware</a>
                 <ul>
@@ -63,5 +64,132 @@
         <a href="../views/servidores.php" class="btn btn-menu">Menú principal</a>
         <a href="../logout.php" class="btn btn-logout">Cerrar sesión</a>
     </div>
-
 </nav>
+
+<style>
+    /* Reset básico */
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    .navbar {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background: #1f1f1f;
+        color: white;
+        padding: 10px 20px;
+        position: relative;
+    }
+
+    .navbar .logo {
+        font-size: 1.5em;
+        font-weight: bold;
+    }
+
+    .navbar .menu ul {
+        display: flex;
+        list-style: none;
+    }
+
+    .navbar .menu ul li {
+        position: relative;
+    }
+
+    .navbar .menu ul li a {
+        display: block;
+        padding: 10px 15px;
+        text-decoration: none;
+        color: white;
+    }
+
+    .navbar .menu ul li ul {
+        position: absolute;
+        top: 100%;
+        left: 0;
+        display: none;
+        background: #333;
+        flex-direction: column;
+        min-width: 200px;
+    }
+
+    .navbar .menu ul li:hover>ul {
+        display: flex;
+    }
+
+    .navbar .menu ul li ul li a {
+        padding: 10px;
+    }
+
+    .navbar .actions {
+        display: flex;
+        gap: 10px;
+    }
+
+    .navbar .btn {
+        padding: 7px 15px;
+        text-decoration: none;
+        color: white;
+        border-radius: 5px;
+        background-color: #007bff;
+    }
+
+    .navbar .btn-logout {
+        background-color: #e74c3c;
+    }
+
+    /* Hamburguesa */
+    .hamburger {
+        display: none;
+        font-size: 1.8em;
+        cursor: pointer;
+    }
+
+    /* Responsive */
+    @media (max-width: 900px) {
+        .navbar {
+            flex-wrap: wrap;
+        }
+
+        .hamburger {
+            display: block;
+        }
+
+        .menu {
+            width: 100%;
+            display: none;
+            flex-direction: column;
+        }
+
+        .menu ul {
+            flex-direction: column;
+        }
+
+        .menu ul li ul {
+            position: relative;
+        }
+
+        .menu ul li:hover>ul {
+            display: none;
+        }
+
+        .menu.show {
+            display: flex;
+        }
+
+        .actions {
+            width: 100%;
+            justify-content: flex-start;
+            margin-top: 10px;
+        }
+    }
+</style>
+
+<script>
+    function toggleMenu() {
+        const menu = document.getElementById('menu');
+        menu.classList.toggle('show');
+    }
+</script>
